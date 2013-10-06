b241ad2828efa682f0a705f784c02553cd695edc
louisdion <maden.ldm@gmail.com>
2013-10-05 23:35:56 -0400

    Completed IRC interface + controller

---
+++ irc-client/EventNotifier.java
@@ -0,0 +1,19 @@
+// public class EventNotifier
+// {
+//     private IRCEvent ie;
+//     private boolean somethingHappened; 
+//     public EventNotifier (IRCEvent event)
+//     {
+// 	// Save the event object for later use.
+// 	ie = event; 
+// 	// Nothing to report yet.
+// 	somethingHappened = false;
+//     } 
+
+//     public void doWork ()
+//     {
+// 	System.out.println("dowork");
+//         ie.interestingEvent ("foo");
+//     }
+// }
+
